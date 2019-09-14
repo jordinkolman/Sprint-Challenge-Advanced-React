dab428e15e711179b9f36141b4f8654228f75b8d
Jordin Kolman <git@github.com>
2019-09-14 09:57:09 -0500

    Finished MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NavBar from './components/NavBar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import PlayerCard from "./components/PlayerCard.js";
+
+class App extends React.Component {
+  state = {
+    players: []
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:5000/api/players")
+      .then(res => res.json())
+      .then(data => this.setState({ players: data }))
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <div id="testId" className="cards">
+          {this.state.players.map(player => (
+            <PlayerCard key={player.id} player={player} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
